abbae5c22110c2e8fedba0a10473939628db958a
csfilipinyi <csaba.filipinyi@gmail.com>
2020-08-07 21:02:25 +0100

    Initial commit

---
+++ app-simple-routing.js
@@ -0,0 +1,77 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+let movies = [
+  {
+    id: 1,
+    title: "Medici - The Magnificent",
+    category: "show"
+  },
+  {
+    id: 2,
+    title: "Inception",
+    category: "movie"
+  },
+  {
+    id: 3,
+    title: "The Martian",
+    category: "movie"
+  },
+  {
+    id: 4,
+    title: "Extraction",
+    category: "movie"
+  },
+  {
+    id: 5,
+    title: "Money Heist",
+    category: "show"
+  },
+
+
+]
+
+app.get('/movies', (req, res) => {
+  let category = req.query.cat
+  let limit = req.query.limit
+  let hits = movies
+  if (category !== undefined) {
+    hits = hits.filter(element => element.category === category)
+  }
+  if (limit !== undefined) {
+    hits= hits.slice(0,Number(limit))
+  }
+  res.send(hits)
+})
+
+app.get('/movies/:id', (req, res) => {
+  let id = Number(req.params.id); 
+  let mymovie = movies.find(movie => movie.id === id)
+ // let mymovie = movies.find(findMovie(id))
+  res.send(mymovie)
+})
+
+app.delete('/movies/:id', (req, res) => {
+  let id = Number(req.params.id); 
+  let index = movies.findIndex(movie => movie.id === id)
+  movies.splice(index,1)
+  res.send("ok")
+})
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+let findMovie = function(id) {
+  return function(movie) {
+    return movie.id === id
+  }
+}
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
