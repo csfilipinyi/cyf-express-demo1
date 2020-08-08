38311bd153057cb8b42fe22466265f9ed98d1dd2
csfilipinyi <csaba.filipinyi@gmail.com>
2020-08-08 08:39:15 +0100

    Advanced example added

--- app-advanced-routing.js
+++ app-advanced-routing.js
@@ -1,35 +1,23 @@
 const express = require('express')
+const bodyParser = require("body-parser");
 const app = express()
 const port = 3000
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+function Movie(id, title, category) {
+  this.title = title
+  this.id = id
+  this.category = category
+}
+
 let movies = [
-  {
-    id: 1,
-    title: "Medici - The Magnificent",
-    category: "show"
-  },
-  {
-    id: 2,
-    title: "Inception",
-    category: "movie"
-  },
-  {
-    id: 3,
-    title: "The Martian",
-    category: "movie"
-  },
-  {
-    id: 4,
-    title: "Extraction",
-    category: "movie"
-  },
-  {
-    id: 5,
-    title: "Money Heist",
-    category: "show"
-  },
-
-
+  new Movie(1,"Medici - The Magnificent","movie"),
+  new Movie(2,"Inception","movie"),
+  new Movie(3,"The Martian","movie"),
+  new Movie(4,"Extraction","show"),
+  new Movie(5,"Money Heist","show")
 ]
 
 app.get('/movies', (req, res) => {
@@ -38,10 +26,14 @@ app.get('/movies', (req, res) => {
 
 app.get('/movies/search', (req, res) => {
   let category = req.query.cat
+  let title = req.query.title
   let limit = req.query.limit
   let hits = movies
   if (category !== undefined) {
     hits = hits.filter(element => element.category === category)
+  }
+  if (title !== undefined) {
+    hits = hits.filter(element => element.title.includes(title))
   }
   if (limit !== undefined) {
     hits= hits.slice(0,Number(limit))
@@ -60,6 +52,12 @@ app.delete('/movies/:id', (req, res) => {
   let id = Number(req.params.id); 
   let index = movies.findIndex(movie => movie.id === id)
   movies.splice(index,1)
+  res.send("ok")
+})
+
+app.post('/movies', (req, res) => {
+  let newMovie = req.body;
+  movies.push(newMovie);
   res.send("ok")
 })
 
